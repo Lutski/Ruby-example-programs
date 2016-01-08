0d7705dd2dfc21cfcdc71a3b75f690f4eceff095
Lutski <ludvigsoerensen@gmail.com>
2016-01-08 23:16:38 +0100

    Update classes-inheritance.rb

--- classes-inheritance.rb
+++ classes-inheritance.rb
@@ -1,12 +1,12 @@
 =begin
 
-Inheritance is simple a relation between 2 classes fx I have created a Human class and
-a Ninja class, that inherits features/methods from the Human class. On programming terms we
+Inheritance is simply a relation between 2 classes fx I have created a Human class and
+a Ninja class that inherits features/methods from the Human class. On programming terms we
 will call the Human class the superclass and the Ninja class the subclass since
 that is the class that inherits the superclass's features/methods etc.
 
 As you can see in the code, the Ninja class (subclass) is able to access the Human class (superclass)
-methods and even edit them so they work better inside of the Ninja class or even use them without
+methods and edit them so they work better inside of the Ninja class, or even use them without
 changing them. You can see examples of all this when you read through the code and run the program.
 
 You are also able to edit/create the initialize method in your subclass.
